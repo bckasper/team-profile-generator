4f7c3ff9e7490c0d6ee09a8db1529b5625ae6784
Ben Kasper <bckasper@gmail.com>
2021-10-06 21:37:45 -0400

    added requirements to the index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+// Requirements for this script to run
+const inquirer = require("inquirer")
+const fs = require("fs")
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+
+
